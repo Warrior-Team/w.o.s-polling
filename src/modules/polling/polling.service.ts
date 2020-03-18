ffa1249198f0f2a1146211e5522ea985111a0b9e
michalaei <michalaei@gmail.com>
2020-03-18 15:56:36 +0200

    adding notification when server went up or went it went down

--- src/modules/polling/polling.service.ts
+++ src/modules/polling/polling.service.ts
@@ -50,24 +50,62 @@ export class PollingService {
             try {
                 let response = await axios.get(server.url);
             } catch (error) {
-                let query = `SELECT "isAlive" from reality${reality.warriorReality}.servers where id = ${server.id}`;
-                let isAliveData = await DbConnector.query(query);
-                let isAlive = isAliveData.rows[0].isAlive;
-                if (isAlive) {
-                    SocketsManager.sendMessage({server: server.name, reality: reality, status: 'down'});
-                    console.error(`Polling stage - failed getting answer from ${server.name} in reality ${reality.name}`);
-                    let updateQuery = `update reality${reality.warriorReality}.servers SET "isAlive"=false where id = ${server.id}`;
-                    await DbConnector.query(updateQuery);
-                }
+                await this.handleServerDown(reality, server);
                 throw new Error(error);
             }
             let endTime = Date.now();
+            await this.handleServerUp(reality, server);
             let duration = endTime - startTime;
             console.log(`Polling stage - Reality : ${reality.name}, to Server: ${server.name}`);
             await DbConnector.query(`INSERT INTO reality${reality.warriorReality}.statistics (server_id, request_time, duration_ms) 
                                     values (${server.id},now(),${duration} ) `);
         } catch (error) {
             console.error(`Error occurred - ${error}`);
+        }
+    }
+
+    private static async handleServerDown(reality: IReality, server: IServer) {
+        let query = `SELECT "isAlive" from reality${reality.warriorReality}.servers where id = ${server.id}`;
+        let isAliveData = await DbConnector.query(query);
+        let isAlive = isAliveData.rows[0].isAlive;
+        if (isAlive) {
+            SocketsManager.sendMessage({
+                type: 'notification',
+                server: server.name,
+                reality: reality,
+                status: 'down'
+            });
+            console.error(`Polling stage - failed getting answer from ${server.name} in reality ${reality.name}`);
+            let updateQuery = `update reality${reality.warriorReality}.servers SET "isAlive"=false where id = ${server.id}`;
+            await DbConnector.query(updateQuery);
+            SocketsManager.sendMessage({
+                type: 'update',
+                server: server.name,
+                reality: reality,
+                data: {id: server.id, isAlive: false}
+            });
+        }
+    }
+
+    private static async handleServerUp(reality: IReality, server: IServer) {
+        let query = `SELECT "isAlive" from reality${reality.warriorReality}.servers where id = ${server.id}`;
+        let isAliveData = await DbConnector.query(query);
+        let isAlive = isAliveData.rows[0].isAlive;
+        if (!isAlive) {
+            SocketsManager.sendMessage({
+                type: 'notification',
+                server: server.name,
+                reality: reality,
+                status: 'up'
+            });
+            let updateQuery = `update reality${reality.warriorReality}.servers SET "isAlive"=true where id = ${server.id}`;
+            await DbConnector.query(updateQuery);
+            SocketsManager.sendMessage({
+                type: 'update',
+                server: server.name,
+                reality: reality,
+                data: {id: server.id, isAlive: true}
+            });
         }
     }
 }
